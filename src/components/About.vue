26fa1d48069ead25b65c416b5263215543a68085
lorddennisjoplin <lorddennisjoplin@gmail.com>
2026-02-20 15:16:11 +0800

    Add link

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,9 @@
 	<section id="landing" class="mt-5 p-5 rounded">
 		<div class="row align-items-center">
 			<div class="col-md-3 text-center p-md-4 p-0">
-				<img src="https://i.imgur.com/L1uGLx4.png" class="rounded-circle img-fluid" width="200" height="200" alt="Lord Acuña Profile Photo">
+				<a href="https://lordaroundtheworld.com" target="_blank">
+					<img src="https://i.imgur.com/L1uGLx4.png" class="rounded-circle img-fluid" width="200" height="200" alt="Lord Acuña Profile Photo">
+				</a>
 			</div>
 			<div class="col-md-9 pt-4 px-4 pb-0 p-md-4">
 				<h1 class="text-center text-md-start">Lord Acuña</h1>
